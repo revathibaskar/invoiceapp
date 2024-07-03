b866531770ba5c9318ce4710edd6855dc5a55f1e
revathibaskar <revathibaskar2003@gmail.com>
2024-07-03 14:42:10 +0530

    created basic files

---
+++ invoice-app/src/App.js
@@ -0,0 +1,11 @@
+import LoginPage from './components/LoginPage';
+import './App.css';
+
+function App() {
+  return (
+    <LoginPage />
+  )
+    
+}
+
+export default App;
---
+++ invoice-app/src/components/LoginPage/index.js
@@ -0,0 +1,7 @@
+const LoginPage = () => {
+         return(
+            <h1>hi</h1>
+         )
+}
+
+export default LoginPage
